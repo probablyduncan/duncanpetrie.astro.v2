d0f85e27874a27cf0028ad637f87430c9b9ad0d2
probablyduncan <duncanpetrie1@gmail.com>
2025-02-09 19:51:06 +0000

    upgrade astro and tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
 import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [mdx()],
   site: 'https://duncanpetrie.com/',
+
   prefetch: {
     prefetchAll: true
   },
+
   experimental: {
     contentIntellisense: true,
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
